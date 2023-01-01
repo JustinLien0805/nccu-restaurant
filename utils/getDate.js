d1541486ee27e522119d12a7b8019ee05538a01a
JustinLien0805 <6998dragon@gmail.com>
2023-01-02 00:21:57 +0800

    format date v2

--- utils/getDate.js
+++ utils/getDate.js
@@ -1,6 +1,17 @@
-const date = new Date();
-date.setDate(date.getDate() + 1);
-const month = date.getMonth() + 1;
-const day = date.getDate();
-export const dateStr = `${month} ${day}`;
+var date = new Date();
+
+var year = date.getFullYear();
+var month = date.getMonth() + 1; // getMonth() returns a 0-based value, so we need to add 1
+var day = date.getDate();
+
+if (month < 10) {
+  month = "0" + month; // add a leading zero if necessary
+}
+
+if (day < 10) {
+  day = "0" + day; // add a leading zero if necessary
+}
+
+export const dateStr = year + "-" + month + "-" + day;
+
 export const formatedDate = `${month}月${day}日`;
