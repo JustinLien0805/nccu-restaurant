8341279b97b7bdf11103b70d11d965c8b86ff0c8
JustinLien0805 <6998dragon@gmail.com>
2023-01-05 19:05:01 +0800

    new build

--- pages/api/user/signUp.js
+++ pages/api/user/signUp.js
@@ -6,21 +6,21 @@ export default async function handler(req, res) {
   const { studentId, password, gender, occupation } = req.body;
   console.log(studentId, password, gender, occupation);
   try {
-    // const userExists = await checkIfUserExists(studentId);
-    // if (userExists) {
-    //   res.json({ error: "User already exists" });
-    //   return;
-    // } else {
-    const user = await prisma.user.create({
-      data: {
-        studentId: studentId,
-        Password: password,
-        gender: gender,
-        occupation: occupation,
-      },
-    });
-    res.json(user);
-    // }
+    const userExists = await checkIfUserExists(studentId);
+    if (userExists) {
+      res.json({ error: "User already exists" });
+      return;
+    } else {
+      const user = await prisma.user.create({
+        data: {
+          studentId: studentId,
+          Password: password,
+          gender: gender,
+          occupation: occupation,
+        },
+      });
+      res.json(user);
+    }
   } catch (err) {
     res.json({ error: err.message });
   }
